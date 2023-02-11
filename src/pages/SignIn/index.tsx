61d0dbeb6a23dcf79755ab22fbcc4e703dbfa033
Felipe Godoy <felipe@fgfinfo.com.br>
2023-02-11 17:18:46 -0300

    public appointments manager 4

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import React, { useRef, useCallback } from "react";
-import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+//import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import {  FiMail, FiLock } from "react-icons/fi";
 import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
 import * as Yup from "yup";
-import { Link, useHistory } from "react-router-dom";
+//import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 
 import { useAuth } from "../../hooks/auth";
